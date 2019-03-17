a5890589b469151d23c53023b898eeb058e6d557
Teyim Pila <teyim.pila@gmail.com>
2019-03-17 09:39:07 +0100

    complete a second implementation of day 2

--- day 02/problem_02.py
+++ day 02/problem_02.py
@@ -3,7 +3,8 @@ Good morning! Here's your coding interview problem for today.
 
 This problem was asked by Uber.
 
-Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+Given an array of integers, return a new array such that each element at index i
+of the new array is the product of all the numbers in the original array except the one at i.
 
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
@@ -19,16 +20,40 @@ def resulting_product_arr(arr):
         product *= i
 
     for j in range(len(arr)):
-        print(j)
         result.append(product // arr[j])
 
     return result
 
 
-output = resulting_product_arr([1, 2, 3, 4, 5])
+print(resulting_product_arr([1, 2, 3, 4, 5]))
 
-print(output)
 
 """
-To be continued: what if you can't use division?
+What if you can't use division?
 """
+
+
+def prod_above_index(i, arr):
+    product = 1
+    for i in range((i + 1), len(arr)):
+        product *= arr[i]
+    return product
+
+
+def prod_before_index(index, arr):
+    product = 1
+    for i in range(index):
+        product *= arr[i]
+    return product
+
+
+def products_of_rest(arr):
+    result = []
+    for i in range(len(arr)):
+        prod_before = prod_before_index(i, arr)
+        prod_after = prod_above_index(i, arr)
+        result.append(prod_before * prod_after)
+    return result
+
+
+print(products_of_rest([1, 2, 3, 4, 5]))
