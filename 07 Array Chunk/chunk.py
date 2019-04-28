88489484011490c3b746fe86cf90d52dcf2434e8
Teyim Pila <teyim.pila@gmail.com>
2019-04-28 12:02:54 +0100

    implement version 2 of array chunk

--- 07 Array Chunk/chunk.py
+++ 07 Array Chunk/chunk.py
@@ -25,3 +25,17 @@ result = chunk([1, 2, 3, 4], 1)
 
 print(result)
 
+
+def chunk_v2(arr, chunk_size):
+    chunk_count = math.ceil(len(arr) / chunk_size)
+    output = []
+    for i in range(chunk_count):
+        _chunk = arr[: chunk_size]
+        output.append(_chunk)
+        del (arr[: chunk_size])
+    return output
+
+
+result = chunk_v2([1, 2, 3, 4], 8)
+
+print(result)
