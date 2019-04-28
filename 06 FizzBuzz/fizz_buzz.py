98bf0bde9645a3c1bbc7a116d53185b815f6e957
Teyim Pila <teyim.pila@gmail.com>
2019-04-28 10:15:47 +0100

    clean code

--- 06 FizzBuzz/fizz_buzz.py
+++ 06 FizzBuzz/fizz_buzz.py
@@ -4,9 +4,9 @@ If any of the numbers is a multiple of 3, print fizz, it is a multiple
 of 5, print buzz if it is a multiple of 5 and 5, print fizzbuzz
 """
 
-def fizz_buzz(n):
 
-    for number in range(1, n+1):
+def fizz_buzz(n):
+    for number in range(1, n + 1):
         if number % 3 == 0 and number % 5 != 0:
             print("{0} ==> fizz".format(number))
         if number % 5 == 0 and number % 3 != 0:
